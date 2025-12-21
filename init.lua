cf3501983d07834cf3758007f3c285bd03fdd8dd
santomon <simon.bohlender@kgu.de>
2025-12-21 12:03:00 +0100

    updated init.lua maybe

--- init.lua
+++ init.lua
@@ -2,12 +2,12 @@ require("config.lazy")
 require("santomon")
 
 vim.filetype.add({
-  filename = {
-    ['Tiltfile'] = 'tiltfile',
-  },
+    filename = {
+        ['Tiltfile'] = 'tiltfile',
+    },
 })
 
-vim.cmd[[set completeopt+=menuone,noselect,popup,]]
+vim.cmd [[set completeopt+=menuone,noselect,popup,]]
 
 
 vim.lsp.enable("basedpyright")
@@ -18,3 +18,18 @@ vim.lsp.enable("cssls")
 vim.lsp.enable("yamlls")
 vim.lsp.enable("ts_ls")
 vim.lsp.enable("tilt_ls")
+
+-- Optional: Only required if you need to update the language server settings
+-- vim.lsp.config('ty', {
+--   settings = {
+--     ty = {
+--       -- ty language server settings go here
+--     }
+--   }
+-- })
+--
+-- Required: Enable the language server
+-- vim.lsp.enable("ty")
+
+
+vim.lsp.enable("ruff")
