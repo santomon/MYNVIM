b7d41a21e51c8e72cd028e44a76e8245bf853ba1
santomon <simon.bohlender@kgu.de>
2025-12-21 11:56:18 +0100

    for now, not using transparent backgrounds to not get distracted so much

--- after/plugin/catppuccin.lua
+++ after/plugin/catppuccin.lua
@@ -4,13 +4,13 @@ require("catppuccin").setup({
         light = "latte",
         dark = "mocha",
     },
-    transparent_background = true, -- disables setting the background color.
+    transparent_background = false, -- disables setting the background color.
     float = {
-        transparent = true, -- enable transparent floating windows
+        transparent = false, -- enable transparent floating windows
         solid = true, -- use solid styling for floating windows, see |winborder|
     },
     show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
-    term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
+    term_colors = true, -- sets terminal colors (e.g. `g:terminal_color_0`)
     dim_inactive = {
         enabled = false, -- dims the background color of inactive window
         shade = "dark",
