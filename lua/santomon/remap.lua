5a8b4d1592795bdde6f2eecb5bfd2301addb5696
santomon <simon.bohlender@kgu.de>
2025-12-21 02:14:30 +0100

    delete line, where we accidentally overwrote C-u

--- lua/santomon/remap.lua
+++ lua/santomon/remap.lua
@@ -12,7 +12,6 @@ vim.keymap.set("n", "<C-d>", "10jzz")
 vim.keymap.set("n", "<M-S-u>", "<C-u>zz")
 vim.keymap.set("n", "<M-S-d>", "<C-d>zz")
 
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 vim.keymap.set("x", "<leader>p", "\"_dP")
